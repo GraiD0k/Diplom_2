167d17d25d3b19008e6e325dd913fc97079a8f8f
Alekseev <darkflashka@yandex.ru>
2025-07-16 22:56:41 +0300

    test

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from faker import Faker
+
+@pytest.fixture()
+def generate_random_email():
+    fake = Faker()
+    email = fake.email()
+    return email
+
+@pytest.fixture()
+def generate_random_password():
+    fake = Faker()
+    password = fake.password(length=10)
+    return password
+
+@pytest.fixture()
+def generate_random_name():
+    fake = Faker()
+    name = fake.name()
+    return name
